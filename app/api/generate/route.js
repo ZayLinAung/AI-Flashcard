aa3f8701bad1e7b41ae23654b598eb17a1517eb0
Danny Liang <dannyliang2005@gmai.com>
2024-08-12 23:55:59 -0400

    download dependencies

---
+++ app/api/generate/route.js
@@ -0,0 +1,4 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `You are a flashcard creator`
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZBSXUCSy2od4YMwDnj7pCoxvxSpaQ6VA",
+  authDomain: "flashcard-saas-5d088.firebaseapp.com",
+  projectId: "flashcard-saas-5d088",
+  storageBucket: "flashcard-saas-5d088.appspot.com",
+  messagingSenderId: "961681056071",
+  appId: "1:961681056071:web:ec0fc3d444af3a21deec62",
+  measurementId: "G-8FGRNCS954"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
