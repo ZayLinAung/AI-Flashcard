73f1e087f66d59239b0a3728358745f7008a967a
Danny Liang <dannyliang2005@gmai.com>
2024-08-17 12:30:26 -0400

    complete flashcard with gemini api

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,4 +1,41 @@
 import { NextResponse } from "next/server";
-import OpenAI from "openai";
+import GoogleGenerativeAI from "gemini-ai";
 
-const systemPrompt = `You are a flashcard creator`
+const systemPrompt = `You are a flashcard creator
+json format:
+{
+    "flashcard": [{
+        "front": str,
+        "back": str
+    }]
+}
+`;
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+export async function POST(req) {
+  const data = await req.text();
+  const prompt = systemPrompt + data;
+
+  let model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    generationConfig: { responseMimeType: "application/json" },
+  });
+  let result = await model.generateContent({
+    contents: [
+      {
+        role: "model",
+        parts: [{ text: prompt }],
+      },
+      {
+        role: "user",
+        parts: [{ text: data }],
+      },
+    ],
+  });
+
+  const text = result.response.text();
+  const flashcards = JSON.parse(text);
+
+  return NextResponse.json(flashcards.flashcard);
+}
